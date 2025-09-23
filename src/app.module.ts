f3f2d4fe93a674cf6c39957a6ef22f7c52cc25ed
Gab27x <gabrielernestoeb@outlook.com>
2025-09-23 14:38:32 -0500

    Add: ci pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,37 +5,31 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 type SupportedDbTypes =
-    | 'mysql'
-    | 'postgres'
-    | 'sqlite'
-    | 'mariadb'
-    | 'mongodb'
-    | 'oracle';
+  | 'mysql'
+  | 'postgres'
+  | 'sqlite'
+  | 'mariadb'
+  | 'mongodb'
+  | 'oracle';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true}),
-    
+    ConfigModule.forRoot({ isGlobal: true }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type:  configService.get<SupportedDbTypes>('DB_TYPE') ?? 'postgres' ,
-        host: configService.get<string>('DB_HOST') ?? 'localhost' ,
+        type: configService.get<SupportedDbTypes>('DB_TYPE') ?? 'postgres',
+        host: configService.get<string>('DB_HOST') ?? 'localhost',
         port: configService.get<number>('DB_PORT') ?? 5432,
         username: configService.get<string>('DB_USERNAME') ?? 'root',
         password: configService.get<string>('DB_PASSWORD') ?? 'root',
         database: configService.get<string>('DB_DATABASE') ?? 'vet',
-        synchronize:  configService.get<boolean>('DB_SYNCHRONIZE') ?? false,
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE') ?? false,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-
-        
-          
       }),
-
     }),
-
-
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    // Basic Swagger configuration
+  // Basic Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Veterinary API')
     .setDescription('API documentation for the veterinary clinic')
