c4b349a38342d9b458fb36c079a617423e31ed07
Gab27x <gabrielernestoeb@outlook.com>
2025-09-23 10:17:37 -0500

    Add: swagger and its config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Basic Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Veterinary API')
+    .setDescription('API documentation for the veterinary clinic')
+    .setVersion('1.0')
+    .build();
+
+  // Create Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Serve Swagger UI at /api
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
