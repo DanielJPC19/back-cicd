940f71d445454dff671faf15383e7b00b7642b64
Gab27x <gabrielernestoeb@outlook.com>
2025-10-08 17:53:28 -0500

    feat(auth): jwt usage to postman tests

--- src/core/auth/dto/update-user-role.dto.ts
+++ src/core/auth/dto/update-user-role.dto.ts
@@ -3,16 +3,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty } from 'class-validator';
 
-/**
- * DTO para la petición de cambio de rol de un usuario.
- */
+
 export class SetUserRoleDto {
   @ApiProperty({
   	description: 'El ID del nuevo rol que se asignará al usuario.',
-  	example: 4, // El ID de un rol, por ejemplo, 'Administrador'
+  	example: 1, 
   	type: Number,
   })
-  @IsNotEmpty({ message: 'El role_id es obligatorio.' })
-  @IsInt({ message: 'El role_id debe ser un número entero.' })
+  @IsNotEmpty()
+  @IsInt()
   	roleId: number;
 }
