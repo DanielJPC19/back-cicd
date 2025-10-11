8a394d68d31d5f6e226898754274c78b70b01211
Gab27x <gabrielernestoeb@outlook.com>
2025-10-11 18:58:41 -0500

    feat(auth): add unit test for users  service and controller

--- src/core/auth/dto/update-user.dto.ts
+++ src/core/auth/dto/update-user.dto.ts
@@ -1,14 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @ApiProperty({
-    	description: 'Correo electrónico del usuario',
-    	example: 'juan.perez@example.com',
-    })
-    @IsEmail()
-    @IsNotEmpty()
-    	email: string;
 }
