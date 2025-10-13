0b96a1b6d6e30422e4e48dd9b14110e0ba087848
Andres Chamorro <chamorroricardo29@gmail.com>
2025-10-13 11:22:05 -0500

    refactor: separate catalogs from clinic module

---
+++ src/catalogs/catalogs.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { DiagnosticTypesModule } from './diagnostic-types/diagnostic-types.module';
+import { SpeciesModule } from './species/species.module';
+
+@Module({
+  imports: [
+    SpeciesModule,
+    DiagnosticTypesModule,
+  ],
+  exports: [
+    SpeciesModule,
+    DiagnosticTypesModule,
+  ],
+})
+export class CatalogsModule {}
